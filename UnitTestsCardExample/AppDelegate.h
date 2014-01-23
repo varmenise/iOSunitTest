bb0596ee2b7e4f0014828bfdad58d5710dd29d2c
steve <stevewessels@me.com>
2014-01-23 13:02:16 -0600

    Initial Commit

---
+++ UnitTestsCardExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UnitTestsCardExample
+//
+//  Created by Stephan B Wessels on 1/23/14.
+//  Copyright (c) 2014 Plum Street Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UnitTestsCardExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UnitTestsCardExample
+//
+//  Created by Stephan B Wessels on 1/23/14.
+//  Copyright (c) 2014 Plum Street Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
